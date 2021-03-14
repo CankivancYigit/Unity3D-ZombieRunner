510cf31cfdaf4a82392adb21cbbd33f7644a79f3
CankivancYigit <CankivancYigit@github.com>
2021-03-14 06:20:12 +0300

    FlashLight Added To Player.

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float angleDecay = 0.5f;
+    [SerializeField] float minSpotAngle = 40f;
+
+    [SerializeField] float intensityDecay = 0.1f;
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseSpotAngle();
+        DecreaseIntensity();
+    }
+
+    void DecreaseSpotAngle()
+    {
+        if (myLight.spotAngle <= minSpotAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }    
+    }
+
+    void DecreaseIntensity()
+    {
+        myLight.intensity -= intensityDecay * Time.deltaTime;
+    }
+}
