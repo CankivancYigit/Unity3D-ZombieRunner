0aa10bffaef1ff9944d0c87e8e4237753e7d4d81
CankivancYigit <CankivancYigit@github.com>
2021-03-16 05:01:14 +0300

    Blood Splatter Added For Displaying When Damage Taking.

---
+++ Assets/Scripts/DisplayDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDamage : MonoBehaviour
+{
+    [SerializeField] Canvas damageDisplay;
+    [SerializeField] float damageDisplayingTime = 0.3f;
+   
+    void Start()
+    {
+        damageDisplay.enabled = false;
+    }
+
+    public void ShowDamage()
+    {
+        StartCoroutine(ShowSplatter());
+    }
+
+    IEnumerator ShowSplatter()
+    {
+        damageDisplay.enabled = true;
+        yield return new WaitForSeconds(damageDisplayingTime);
+        damageDisplay.enabled = false;
+    }
+}
