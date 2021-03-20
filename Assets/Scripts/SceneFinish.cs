018f706eefd4878e2bb5edee0e5fc0b5e603b815
CankivancYigit <CankivancYigit@github.com>
2021-03-20 07:39:11 +0300

    Scene Finishing Added.

---
+++ Assets/Scripts/SceneFinish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFinish : MonoBehaviour
+{
+    [SerializeField] Canvas finishSceneCanvas;
+    void Start()
+    {
+        finishSceneCanvas.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finishSceneCanvas.enabled = true;
+        Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        AudioSource[] allSounds = FindObjectsOfType<AudioSource>();
+        for (int i = 0; i < allSounds.Length; i++)
+        {
+            allSounds[i].enabled = false;
+        }
+    }
+}
